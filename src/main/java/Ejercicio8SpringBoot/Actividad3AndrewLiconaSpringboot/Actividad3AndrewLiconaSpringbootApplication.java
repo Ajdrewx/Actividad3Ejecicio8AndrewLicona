a0f3cbc30d61ea27c81b46ded4cd153ca7976f09
Andrew José Licona Blanco <136419270+Andrewjlb@users.noreply.github.com>
2024-11-23 23:11:45 -0500

    Estrucctura del Proyecto

---
+++ src/main/java/Ejercicio8SpringBoot/Actividad3AndrewLiconaSpringboot/Actividad3AndrewLiconaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package Ejercicio8SpringBoot.Actividad3AndrewLiconaSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Actividad3AndrewLiconaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Actividad3AndrewLiconaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Ejercicio8SpringBoot/Actividad3AndrewLiconaSpringboot/Actividad3AndrewLiconaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package Ejercicio8SpringBoot.Actividad3AndrewLiconaSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Actividad3AndrewLiconaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
